65040edb20ab21908b5e5627103b72cd59ae68bf
Robert Vásquez <vasquezrober09@gmail.com>
2023-04-23 20:46:57 -0400

    Finished menu - html, css and js

---
+++ js/menu.js
@@ -0,0 +1,34 @@
+const hamburguerIcon = document.querySelector('.nav__hamburguer');
+
+const navOverlay = document.querySelector('.nav__overlay');
+
+hamburguerIcon.addEventListener('click', () => {
+  hamburguerIcon.classList.toggle('nav__hamburguer--open');
+
+  navOverlay.classList.toggle('nav__overlay--show');
+});
+
+navOverlay.addEventListener('click', (e) => {
+  e.preventDefault();
+  const currentElement = e.target;
+  // console.log(e.target.classList.value );
+
+  if(isActive(currentElement, "nav__parent")){
+
+    const subMenu = currentElement.parentElement.children[1];
+
+    console.log(subMenu);
+
+    if(window.innerWidth < 768){
+      // console.log(subMenu.clientHeight);
+      let height = (subMenu.clientHeight == 0) ? subMenu.scrollHeight : 0;
+      subMenu.style.height = `${height}px`;
+    }else {
+
+    }
+  }
+});
+
+const isActive = (element, string) => {
+  return element.classList.value.includes(string);
+}
